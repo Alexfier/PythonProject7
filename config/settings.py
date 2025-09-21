e9e11e68152947103b3c1c9bbf01c9afc784d0e0
oldbe <oldberg1999@gmail.com>
2025-09-21 18:51:07 +0300

    4

--- config/settings.py
+++ config/settings.py
@@ -187,10 +187,15 @@ DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 STRIPE_API_KEY = os.getenv('STRIPE_API_KEY')
 
 REST_FRAMEWORK = {
-    'DEFAULT_RENDERER_CLASSES': [
-        'rest_framework.renderers.JSONRenderer',
+    "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"],
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ),
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
     ],
-    # другие настройки...
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 10,
 }
 
 SIMPLE_JWT = {
